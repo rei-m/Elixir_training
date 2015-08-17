465745641d28c5a59c91257f05a77fc2cf21f9d3
rei-m <star.oceans.rm@wj8.so-net.ne.jp>
2015-08-17 21:48:40 +0900

    add another fizzbuzz

--- neta/fizzbuzz.ex
+++ neta/fizzbuzz.ex
@@ -1,5 +1,5 @@
 defmodule FizzBuzz do
-  def fizz_buzz(n) do
+  def fizz_buzz_1(n) do
     Enum.map_join 1..n, fn x ->
       cond do
         rem(x, 15) === 0 ->
@@ -13,6 +13,25 @@ defmodule FizzBuzz do
       end
     end
   end
+
+  defp fizzbuzz do
+    fn
+      {true, true}   -> "FizzBuzz"
+      {true, false}  -> "Fizz"
+      {false, true}  -> "Buzz"
+      {false, false} -> nil
+    end
+  end
+
+  def fizz_buzz_2(cnt) do
+    1..cnt |> Enum.map_join(fn n ->
+      case fizzbuzz.({rem(n, 3) === 0, rem(n, 5) === 0}) do
+        nil -> n
+        s -> s
+      end
+    end)
+  end
 end
 
-IO.inspect FizzBuzz.fizz_buzz(100)
+IO.inspect FizzBuzz.fizz_buzz_1(30)
+IO.inspect FizzBuzz.fizz_buzz_2(30)
