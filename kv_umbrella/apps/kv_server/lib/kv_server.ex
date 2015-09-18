d9daccd2d1d8233c114b6ad41406c66f1ddf8074
rei-m <star.oceans.rm@wj8.so-net.ne.jp>
2015-09-18 12:56:37 +0900

    add command

--- kv_umbrella/apps/kv_server/lib/kv_server.ex
+++ kv_umbrella/apps/kv_server/lib/kv_server.ex
@@ -1,19 +1,85 @@
 defmodule KVServer do
   use Application
 
-  # See http://elixir-lang.org/docs/stable/elixir/Application.html
-  # for more information on OTP Applications
+  @doc false
   def start(_type, _args) do
-    import Supervisor.Spec, warn: false
+    import Supervisor.Spec
 
     children = [
-      # Define workers and child supervisors to be supervised
-      # worker(KVServer.Worker, [arg1, arg2, arg3])
+      supervisor(Task.Supervisor, [[name: KVServer.TaskSupervisor]]),
+      worker(Task, [KVServer, :accept, [4040]])
     ]
 
-    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: KVServer.Supervisor]
     Supervisor.start_link(children, opts)
   end
+
+  @doc """
+  Starts accepting connections on the given `port`.
+  """
+  def accept(port) do
+    # The options below mean:
+    #
+    # 1. `:binary` - receives data as binaries (instead of lists)
+    # 2. `packet: :line` - receives data line by line
+    # 3. `active: false` - blocks on `:gen_tcp.recv/2` until data is available
+    # 4. `reuseaddr: true` - allows us to reuse the address if the listener crashes
+    #
+    {:ok, socket} = :gen_tcp.listen(port,
+                      [:binary, packet: :line, active: false, reuseaddr: true])
+    IO.puts "Accepting connections on port #{port}"
+    loop_acceptor(socket)
+  end
+
+  # defp loop_acceptor(socket) do
+  #   {:ok, client} = :gen_tcp.accept(socket)
+  #   serve(client)
+  #   loop_acceptor(socket)
+  # end
+
+  defp loop_acceptor(socket) do
+    {:ok, client} = :gen_tcp.accept(socket)
+    {:ok, pid} = Task.Supervisor.start_child(KVServer.TaskSupervisor, fn -> serve(client) end)
+    :ok = :gen_tcp.controlling_process(client, pid)
+    loop_acceptor(socket)
+  end
+
+  defp serve(socket) do
+    import Pipe
+
+    # msg =
+    #   case read_line(socket) do
+    #     {:ok, data} ->
+    #       case KVServer.Command.parse(data) do
+    #         {:ok, command} ->
+    #           KVServer.Command.run(command)
+    #         {:error, _} = err ->
+    #           err
+    #       end
+    #     {:error, _} = err ->
+    #       err
+    #   end
+
+    msg =
+      pipe_matching x, {:ok, x},
+        read_line(socket)
+        |> KVServer.Command.parse()
+        |> KVServer.Command.run()
+
+    write_line(socket, msg)
+    serve(socket)
+  end
+
+  defp read_line(socket) do
+    :gen_tcp.recv(socket, 0)
+  end
+
+  defp write_line(socket, msg) do
+    :gen_tcp.send(socket, format_msg(msg))
+  end
+
+  defp format_msg({:ok, text}), do: text
+  defp format_msg({:error, :not_found}), do: "NOT FOUND\r\n"
+  defp format_msg({:error, :unknown_command}), do: "UNKNOWN COMMAND\r\n"
+  defp format_msg({:error, _}), do: "ERROR\r\n"
 end
